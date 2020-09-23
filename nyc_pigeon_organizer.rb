4dd952919dceca8d3a1b5d9f03040d79cabf5145
Kevin Grow <kevingrow11@gmail.com>
2020-09-23 23:54:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,7 +24,7 @@ def nyc_pigeon_organizer(data)
    data.each_with_object({}) do |(key, value), final_hash| #(aka new hash)
      value.each do |inner_key, names|
        names.each do |name|
-         
+         if !final_array 
        end 
      end 
    end
