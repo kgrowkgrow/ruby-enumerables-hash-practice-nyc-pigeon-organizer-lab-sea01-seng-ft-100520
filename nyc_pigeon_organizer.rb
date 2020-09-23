c99ffa8130a714946ba407e4761c3d4316d322ee
Kevin Grow <kevingrow11@gmail.com>
2020-09-23 23:35:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
    data.each_with_object({}) do |(key, value), final_hash #(aka new hash)|
-     final_hash[key] = value #(something done to it like + 3)
      binding.pry
+     final_hash[key] = value #(something done to it like + 3)
   end 
   binding.pry
 end  
