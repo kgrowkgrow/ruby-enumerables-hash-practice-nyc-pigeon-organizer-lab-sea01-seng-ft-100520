f32ebcd52004a7653ee8f5029c008845ce8781f3
Kevin Grow <kevingrow11@gmail.com>
2020-09-23 23:38:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -49,6 +49,72 @@ nyc_pigeon_organizer(pigeon_datadata)
 
 
 
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_list = {}
+  pigeon_data.each do |attribute_list, option_list|
+    option_list.each do |option, name_list|
+      name_list.each do |name|
+        pigeon_list[name] = {} #{name => attribute_list} #  
+          #attribute_list => 
+      end 
+    end
+  end 
+  pigeon_list
+  #i think pigeon_list exists at this level, so now I have to grab
+          binding.pry
+end
+
+nyc_pigeon_organizer(pigeon_data)
+
+
+
+
+def nyc_pigeon_organizer(data)
+  # data.each_with_object({}) do |(key, value), final_hash (aka new hash)|
+   #  final_hash [key] = value (something done to it like + 3)
+  end 
+end  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
