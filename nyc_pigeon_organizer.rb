3d5c767712e4cffced9508a1853a6307e39d45e0
Kevin Grow <kevingrow11@gmail.com>
2020-09-23 21:48:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,6 +22,7 @@ pigeon_data = {
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_list = {}
   pigeon_data.each do |attribute_list, option_list|
+    binding.pry
     option_list.each do |option, name_list|
       name_list.each do |name|
         pigeon_list[name] = {}
@@ -33,4 +34,4 @@ def nyc_pigeon_organizer(pigeon_data)
 end
 
 nyc_pigeon_organizer(pigeon_data)
-binding.pry
+
