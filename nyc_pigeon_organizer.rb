9f5b270383dbb4de110e883a1f509eb28925551e
Kevin Grow <kevingrow11@gmail.com>
2020-09-24 00:40:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,25 +1,5 @@
 require 'pry'
 
-pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
-binding.pry
-
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_hash|
     value.each do |inner_key, names|
@@ -34,29 +14,8 @@ def nyc_pigeon_organizer(data)
        end 
     end
   end 
- # binding.pry
 end  
 
-#nyc_pigeon_organizer(pigeon_data)
-
-
-
-
-
-#def nyc_pigeon_organizer(pigeon_data)
- # pigeon_list = {}
- # pigeon_data.each do |attribute_list, option_list|
- #   option_list.each do |option, name_list|
- #     name_list.each do |name|
- #       pigeon_list[name] = {} #{name => attribute_list} #  
- #         #attribute_list => 
- #     end 
- #   end
- # end 
- # pigeon_list
- # i think pigeon_list exists at this level, so now I have to grab
- #         binding.pry
-#end
 
 
 
