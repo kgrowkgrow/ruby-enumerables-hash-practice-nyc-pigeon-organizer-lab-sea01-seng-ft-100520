d5f00656ee0f23f48fc664769a1bfe0276912997
Kevin Grow <kevingrow11@gmail.com>
2020-09-24 00:32:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,18 +21,18 @@ pigeon_data = {
 
 
 def nyc_pigeon_organizer(data)
-   final_results = data.each_with_object({}) do |(key, value), final_hash| 
-     value.each do |inner_key, names|
-       names.each do |name|
-         if !final_hash[name]
-           final_hash[name] = {}
-         end 
-         if !final_hash[name][key]
-          final_hash[name][key] = []
+  final_results = data.each_with_object({}) do |(key, value), final_hash|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_hash[name]
+          final_hash[name] = {}
+        end 
+          if !final_hash[name][key]
+           final_hash[name][key] = []
          end 
        final_hash[name][key].push(inner_key.to_s)
-     end 
-   end
+       end 
+    end
   end 
  # binding.pry
 end  
