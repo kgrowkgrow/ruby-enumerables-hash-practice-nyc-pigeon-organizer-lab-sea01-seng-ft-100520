7cc98eb588ae78b412a2a324034c47fb232dce6f
Kevin Grow <kevingrow11@gmail.com>
2020-09-24 00:03:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,7 +30,7 @@ def nyc_pigeon_organizer(data)
              binding.pry
        end 
        if !final_hash[name][key]
-         
+         final_hash[name][key] = []
      end 
      #here?
    end
